3321b28b3d4f6629ee335d0ca6d9757f923f0ea3
Francesco <francescomartinmail@gmail.com>
2021-01-21 16:41:00 +0100

    start

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container mb-5">
+        <h1>SCHOOL HOME PAGE HERE</h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem id porro dolorum fugit possimus mollitia debitis incidunt aut omnis suscipit assumenda voluptatibus temporibus expedita, sint corporis error, distinctio a modi!</p>
+    </div>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,19 @@
+<header class="mb-4">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="{{ route('homepage') }}">SCHOOL SITE</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('homepage') }}">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Classrooms</a>
+              </li>
+          </ul>
+        </div>
+      </nav>
+</header>
